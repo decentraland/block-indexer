b38fa52d5bc36fecec98af988141568df6c1b60c
Hugo Arregui <hugo@decentraland.org>
2023-01-23 13:35:08 -0300

    fix lint

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,23 +1,23 @@
-import { IMetricsComponent } from "@well-known-components/interfaces"
+import { IMetricsComponent } from '@well-known-components/interfaces'
 
 /**
  * @public
  */
 export const metricsDefinitions = {
   block_indexer_misses: {
-    help: "Amount of misses",
-    type: IMetricsComponent.CounterType,
+    help: 'Amount of misses',
+    type: IMetricsComponent.CounterType
   },
   block_indexer_hits: {
-    help: "Amount of hits",
-    type: IMetricsComponent.CounterType,
+    help: 'Amount of hits',
+    type: IMetricsComponent.CounterType
   },
   block_indexer_rpc_requests: {
-    help: "Amount of RPC requests",
-    type: IMetricsComponent.CounterType,
+    help: 'Amount of RPC requests',
+    type: IMetricsComponent.CounterType
   },
   block_indexer_search_duration_ms: {
     type: IMetricsComponent.HistogramType,
-    help: "Search duration in milliseconds.",
-  },
+    help: 'Search duration in milliseconds.'
+  }
 }
